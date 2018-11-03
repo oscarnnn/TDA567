663c0e195ab67b79db63ea05b80c871fc14c4e19
Ljubo <36992530+SteveLjubs@users.noreply.github.com>
2018-11-03 11:22:50 +0100

    partition 7-10

--- Lab1/Test/WorkScheduleTest.java
+++ Lab1/Test/WorkScheduleTest.java
@@ -13,6 +13,7 @@ public class WorkScheduleTest {
        ws = new WorkSchedule(24);
        test1 = "Lars";
        ws.setRequiredNumber(5,0,23);
+
    }
 
    /* Partition #1
@@ -65,33 +66,39 @@ public class WorkScheduleTest {
 
     /* Partition #7
      * Test for hour.workingEmployees.length == requiredNumber
+            between start and endtime
      */
     @Test
     public void testSomeHourFull(){
-
+        ws.setRequiredNumber(0,0,10);
+        Assert.assertTrue(!ws.addWorkingPeriod(test1,0,23));
     }
-
     /* Partition #8
      * Test for hour.workingEmployees.length != reuiredNumber
+       between start and endtime
      */
     @Test
     public void testNoHourFull(){
-
+       Assert.assertTrue(ws.addWorkingPeriod(test1,0,23));
     }
 
     /* Partition #9
      * Test for hour.workingEmployees.contains(employee)
+     *        between start and endtime
      */
     @Test
     public void testEmployeeAlreadyScheduled(){
-
+      ws.addWorkingPeriod(test1,0,5);
+      Assert.assertFalse(ws.addWorkingPeriod(test1,0,5));
     }
 
     /* Partition #10
      * Test for !hour.workingEmployees.contains(employee)
+     *        between start and endtime
      */
     @Test
     public void testEmployeeNotScheduled(){
+        Assert.assertTrue(ws.addWorkingPeriod(test1,0,5));
 
     }
 }
